62655b6f803bf34c9a96a630ee665f89910566b1
Janella <fullmoonfirefly11@gmail.com>
2024-10-04 18:02:09 -0400

    Add Experience page and basic routing

---
+++ src/AboutMe.jsx
@@ -0,0 +1,18 @@
+// Content: About Me
+export default function AboutMe() {
+  return(
+    <div className="container text-center p-1">
+      <div className="row align-items-center mb-3">
+        <div className="col">
+          <img src="src\assets\img\20230218_170648 icon.jpg" className="center-block rounded" width={150}/>
+        </div>
+      </div>
+        <div className="row">
+          <div className="col">
+            <h2><b>I&apos;m Janella Marie Co.</b></h2>
+            <p>A full-stack software engineer currently based in Lutz, FL, USA. I graduated from University of South Florida with Bachelor&apos;s degree in Computer Science. I love building web experiences and I&apos;m always eager to learn new things. </p>
+          </div>
+        </div>
+      </div>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,39 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
 import App from './App.jsx'
+import AboutMe from './AboutMe.jsx';
+import Experience from './Experience.jsx';
+
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path:"",
+        element: <AboutMe />
+      },
+      {
+        path:"AboutMe",
+        element: <AboutMe />
+      },
+      {
+        path:"Experience",
+        element: <Experience />
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
